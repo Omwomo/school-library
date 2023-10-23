28f5e4150b46fa26d628ab2e4b2051033f001b5d
Omwomo <eugeneoduor79@gmail.com>
2023-10-23 17:21:07 +0300

    Passed correc values to the super initializers

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ class Student < Person
   attr_accessor :classroom
 
   def initialize(age:, name: 'Unknown', parent_permission: true, classroom: nil)
-    super(age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
