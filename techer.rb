38ed2bb6c0189d717a60e7444016dcd388b24e30
Omwomo <eugeneoduor79@gmail.com>
2023-10-23 16:52:45 +0300

    Fixed linter error

--- techer.rb
+++ techer.rb
@@ -1,5 +1,5 @@
 class Teacher < Person
-  def initialize(age:, name: 'Unknown', parent_permission: true, specialization:)
+  def initialize(age:, specialization:, name: 'Unknown', parent_permission: true)
     super(age: age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
