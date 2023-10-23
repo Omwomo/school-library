20fb110be73cbba33f52a990ded65275e3af5221
Omwomo <eugeneoduor79@gmail.com>
2023-10-23 16:45:15 +0300

    Created Teacher class which inherited person

---
+++ techer.rb
@@ -0,0 +1,10 @@
+class Teacher < Person
+  def initialize(age:, name: 'Unknown', parent_permission: true, specialization:)
+    super(age: age, name: name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services
+    true
+  end
+end
