517dcc2fa02d96ded3520a42000ab11af0634d71
Omwomo <eugeneoduor79@gmail.com>
2023-10-27 08:22:39 +0300

    Create a method for students belong to a classroom

--- classroom.rb
+++ classroom.rb
@@ -8,7 +8,12 @@ class Classroom
   end
 
   def add_students(student)
-    students << student
+    @students << student
     student.classroom = self
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
