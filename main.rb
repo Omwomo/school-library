63971086f6a658d9c29afccaf9fcfe8aa46b9423
Omwomo <eugeneoduor79@gmail.com>
2023-10-28 17:30:06 +0300

    This code is performing all the functionalities as required but has a linter error

--- main.rb
+++ main.rb
@@ -1,78 +1,48 @@
 require './app'
 
-class LibraryApp
-  def display_options
-    puts 'Options:'
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a person'
-    puts '4. Create a book'
-    puts '5. Create a rental'
-    puts '6. List rentals for a person'
-    puts '7. Quit'
-  end
+def display_options
+  puts 'Options:'
+  puts '1. List all books'
+  puts '2. List all people'
+  puts '3. Create a person'
+  puts '4. Create a book'
+  puts '5. Create a rental'
+  puts '6. List rentals for a person'
+  puts '7. Quit'
+end
 
-  OPTIONS = {
-    1 => :handle_list_books,
-    2 => :handle_list_people,
-    3 => :handle_create_person,
-    4 => :handle_create_book,
-    5 => :handle_create_rental,
-    6 => :handle_list_rentals,
-    7 => :exit
-  }.freeze
-
-  def handle_list_books(books)
+def handle_choice(choice, books, people)
+  case choice
+  when 1
     list_all_books(books)
-  end
-
-  def handle_list_people(people)
+  when 2
     list_all_people(people)
-  end
-
-  def handle_create_person(people)
+  when 3
     create_person(people)
-  end
-
-  def handle_create_book(books)
+  when 4
     create_book(books)
-  end
-
-  def handle_create_rental(books, people)
+  when 5
     create_rental(books, people)
-  end
-
-  def handle_list_rentals(people)
-    print 'Enter person id: '
-    person_id = gets.chomp.to_i
-    list_rentals_for_person(person_id, people)
-  end
-
-  def handle_choice(choice, books, people)
-    option = OPTIONS[choice]
-
-    if option.nil?
-      puts 'Invalid choice. Please try again.'
-    elsif option == :exit
-      exit
-    else
-      send(option, books, people)
-    end
-  end
-
-  def main
-    people = []
-    books = []
-
-    loop do
-      display_options
-
-      choice = gets.chomp.to_i
-
-      handle_choice(choice, books, people)
-    end
+  when 6
+    list_rentals_for_person(people)
+  when 7
+    exit
+  else
+    puts 'Invalid choice. Please try again.'
   end
 end
 
-app = LibraryApp.new
-app.main
+def main
+  people = []
+  books = []
+
+  loop do
+    display_options
+
+    choice = gets.chomp.to_i
+
+    handle_choice(choice, books, people)
+  end
+end
+
+main
