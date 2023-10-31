c57e33295f5cdd4febee0bf067a9b4db239271b4
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-10-31 16:10:15 +0100

    Modify rental.rb

--- rental.rb
+++ rental.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Rental
   attr_accessor :date, :book, :person
 
@@ -8,4 +10,35 @@ class Rental
     book.rentals << self
     person.rentals << self
   end
+
+  # Serialize the object to a hash
+  def to_json
+    {
+      'date' => @date,
+      'person' => @person,
+      'book' => @book,
+    }
+  end
+
+  # Deserialize the hash back into an object
+  def self.from_json(json_hash)
+    self.new(json_hash['date'], json_hash['person'], json_hash['book'])
+  end
+end
+
+def save_rental(rental)
+  File.open('storage/rental.json', 'w') do |file|
+    file.write(JSON.dump(rental))
+  end
+end
+
+def load_rental
+  loaded_rental_data = JSON.parse(File.read('storage/rental.json'))
+  loaded_rental = []
+
+  loaded_rental_data.each do |rental_data|
+    loaded_rental << rental.from_json(rental_data)
+  end
+
+  loaded_rental
 end
