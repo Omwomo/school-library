e31056a90ed72bf442c9646f0fee242f49081b9f
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-11-02 10:00:12 +0100

    Modify teacher_spec.rb

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,14 +1,19 @@
-require_relative 'person'
+require_relative 'spec_helper'
 
-class Teacher < Person
-  attr_accessor :specialization
-
-  def initialize(age:, specialization:, id: nil, name: 'Unknown', parent_permission: true)
-    super(id: id, name: name, age: age, parent_permission: parent_permission)
-    @specialization = specialization
-  end
-
-  def can_use_services
-    true
+describe Teacher do
+  context 'When providing teacher information' do
+    teacher = Teacher.new(age: 40, name: 'Mike', specialization: 'Math')
+    it 'creates a new teacher with the given parameters' do
+      expect(teacher).to be_an_instance_of Teacher
+    end
+    it 'should detect the teacher age' do
+      expect(teacher.age).to eq 40
+    end
+    it 'should detect the teacher name' do
+      expect(teacher.name).to eq 'Mike'
+    end
+    it 'should have always permission to use services' do
+      expect(teacher.can_use_services).to eq true
+    end
   end
 end
