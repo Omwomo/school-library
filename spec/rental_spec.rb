a2a53f4518b31b514fd30637f36a884630c640b0
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-11-02 15:16:29 +0100

    Modify the rental_spec.rb

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,15 +1,34 @@
 require 'spec_helper'
+require 'date'
+require_relative '../rental'
 
 describe Rental do
-  context 'When providing a book and a person' do
-    book = Book.new('East of Eden', 'John Steinbeck')
-    person = Person.new(id: nil, age: 40, name: 'mike', parent_permission: true)
-    rental = Rental.new('18/12/2021', book, person)
-    it 'should create a rental for person' do
-      expect(person.rentals).to include(rental)
+  before :each do
+    @book = Book.new('The Great Gatsby', 'F. Scott Fitzgerald')
+    @person = Person.new(30, 'John Doe', parent_permission: true)
+    @rental_date = Date.new(2023, 11, 1)
+    @rental = Rental.new(@rental_date, @book, @person)
+  end
+
+  describe '#new' do
+    it 'takes three parameters and returns a Rental object' do
+      expect(@rental).to be_an_instance_of(Rental)
     end
-    it 'should create a rental for book' do
-      expect(book.rentals).to include(rental)
+
+    it 'adds the rental to the book and person rentals lists' do
+      expect(@book.rentals).to include(@rental)
+      expect(@person.rentals).to include(@rental)
+    end
+  end
+
+  describe '#rental_to_json' do
+    it 'returns a hash representing the rental' do
+      expected_hash = {
+        date: @rental_date,
+        person: 'John Doe',
+        book: 'The Great Gatsby'
+      }
+      expect(@rental.rental_to_json).to eql(expected_hash)
     end
   end
 end
