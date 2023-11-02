6e66e001dc9cc265a95647c2fa0cc57de001bfcd
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-11-02 07:49:43 +0100

    Add testing units

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,14 @@
+require_relative 'person'
+
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(age:, specialization:, id: nil, name: 'Unknown', parent_permission: true)
+    super(id: id, name: name, age: age, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services
+    true
+  end
+end
